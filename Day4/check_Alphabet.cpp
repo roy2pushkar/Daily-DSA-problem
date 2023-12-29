7a5964e3b0bb48cc17b747101949d24be59c36d8
roy2pushkar <roy2000pushkar@gmail.com>
2023-12-29 16:39:18 +0530

    Daily DSA Problem

---
+++ Day4/check_Alphabet.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout << "Enter the Character";
+    cin >> ch;
+
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z' ))
+    {
+        cout << ch << " is Alphabet : " << endl;
+    }
+    else
+    {
+        cout << ch << " is not a Alphabet :" << endl;
+    }
+}

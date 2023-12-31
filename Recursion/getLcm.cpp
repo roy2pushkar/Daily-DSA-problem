ab3e3d23321d6f448daec87e261fdd83158f9da7
roy2pushkar <roy2000pushkar@gmail.com>
2023-12-31 15:21:18 +0530

    React-Flask-App

---
+++ Recursion/getLcm.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int hcfOfNumber(int num1, int num2)
+{
+    if (num1 == 0)
+    {
+        return num2;
+    }
+    if (num2 == 0)
+    {
+        return num1;
+    }
+    if (num1 == num2)
+    {
+        return num1;
+    }
+    else if (num1 > num2)
+    {
+        return hcfOfNumber(num1 - num2, num2);
+    }
+    else
+    {
+        return hcfOfNumber(num1, num2 - num1);
+    }
+}
+int main()
+{
+    int num1;
+    cout << "Enter first number";
+    cin >> num1;
+    int num2;
+    cout << "Enter second number";
+    cin >> num2;
+    int LCM = (num1 * num2) / hcfOfNumber(num1, num2);
+
+    cout << "LCM of Given two Number is : " << LCM << endl;
+}
